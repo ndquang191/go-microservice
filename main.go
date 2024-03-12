88b7c34fe7ac24918d67a263e40c84818b151468
Quang Nguyen <ndquang191@gmail.com>
2024-03-12 11:22:05 +0700

    basic web server added

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Print("Failed to listen to ser", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
